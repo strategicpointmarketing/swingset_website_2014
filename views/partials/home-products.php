0653e5d3e7ed6f544f2fb5ee0ef10628dc140b40
Trevor Pierce <trevor@lifeblue.com>
2014-02-11 23:02:42 -0600

    updates to home-products.php

--- views/partials/home-products.php
+++ views/partials/home-products.php
@@ -1,72 +1,61 @@
 <div class="relative-container">
 	<div class="wrapper">
-		<ul class="gd-row gt-row unstyled mtn">
-				<li class="gd-third gd-columns gt-half gt-columns pts">
-					<div class="white-bg">
-						<img src="http://placehold.it/600x320">
-
-
-						<div class="pxs">
-							<h3 class="paragon-text primary-heading mtn">Swingsets</h3>
-							<p>Nam pharetra sem eget tempus porttitor. Morbi fringilla vulputate porttitor. In hac habitasse.</p>
-						</div>
+		<ul class="gd-row gt-row media-grid unstyled mtn">
+			<li class="object gd-third gd-columns gt-half gt-columns pts">
+				<figure class="white-bg">
+					<img src="http://placehold.it/600x320">
+					<figcaption class="pxs">
+						<h3 class="paragon-text primary-heading mtn">Swingsets</h3>
+						<p>Nam pharetra sem eget tempus porttitor. Morbi fringilla vulputate porttitor. In hac habitasse.</p>
+					</figcaption>
+				</figure>
+			</li>
+			<li class="object gd-third gd-columns gt-half gt-columns pts">
+				<div class="white-bg">
+					<img src="http://placehold.it/600x320">
+					<div class="pxs">
+						<h3 class="paragon-text primary-heading mtn">Trampolines</h3>
+						<p>Nam pharetra sem eget tempus porttitor. Morbi fringilla vulputate porttitor. In hac habitasse.</p>
+						<p>Nam pharetra sem eget tempus porttitor. Morbi fringilla vulputate porttitor. In hac habitasse.</p>
 					</div>
-				</li>
-				<li class="gd-third gd-columns gt-half gt-columns pts">
-					<div class="white-bg">
-						<img src="http://placehold.it/600x320">
-
-
-						<div class="pxs">
-							<h3 class="paragon-text primary-heading mtn">Trampolines</h3>
-							<p>Nam pharetra sem eget tempus porttitor. Morbi fringilla vulputate porttitor. In hac habitasse.</p>
-						</div>
+				</div>
+			</li>
+			<li class="object gd-third gd-columns gt-half gt-columns pts">
+				<div class="white-bg">
+					<img src="http://placehold.it/600x320">
+					<div class="pxs">
+						<h3 class="paragon-text primary-heading mtn">Basketballs</h3>
+						<p>Nam pharetra sem eget tempus porttitor. Morbi fringilla vulputate porttitor. In hac habitasse.</p>
 					</div>
-				</li>
-				<li class="gd-third gd-columns gt-half gt-columns pts">
-					<div class="white-bg">
-						<img src="http://placehold.it/600x320">
-
-
-						<div class="pxs">
-							<h3 class="paragon-text primary-heading mtn">Basketballs</h3>
-							<p>Nam pharetra sem eget tempus porttitor. Morbi fringilla vulputate porttitor. In hac habitasse.</p>
-						</div>
+				</div>
+			</li>
+			<li class="object gd-third gd-columns gt-half gt-columns pvs">
+				<div class="white-bg">
+					<img src="http://placehold.it/600x320">
+					<div class="pxs">
+						<h3 class="paragon-text primary-heading mtn">Surfacing</h3>
+						<p>Nam pharetra sem eget tempus porttitor. Morbi fringilla vulputate porttitor. In hac habitasse.</p>
 					</div>
-				</li>
-				<li class="gd-third gd-columns gt-half gt-columns pvs">
-					<div class="white-bg">
-						<img src="http://placehold.it/600x320">
-
-
-						<div class="pxs">
-							<h3 class="paragon-text primary-heading mtn">Surfacing</h3>
-							<p>Nam pharetra sem eget tempus porttitor. Morbi fringilla vulputate porttitor. In hac habitasse.</p>
-						</div>
+				</div>
+			</li>
+			<li class="object gd-third gd-columns gt-half gt-columns pvs">
+				<div class="white-bg">
+					<img src="http://placehold.it/600x320">
+					<div class="pxs">
+						<h3 class="paragon-text primary-heading mtn">Sheds</h3>
+						<p>Nam pharetra sem eget tempus porttitor. Morbi fringilla vulputate porttitor. In hac habitasse.</p>
 					</div>
-				</li>
-				<li class="gd-third gd-columns gt-half gt-columns pvs">
-					<div class="white-bg">
-						<img src="http://placehold.it/600x320">
-
-
-						<div class="pxs">
-							<h3 class="paragon-text primary-heading mtn">Sheds</h3>
-							<p>Nam pharetra sem eget tempus porttitor. Morbi fringilla vulputate porttitor. In hac habitasse.</p>
-						</div>
+				</div>
+			</li>
+			<li class="object gd-third gd-columns gt-half gt-columns pvs">
+				<div class="white-bg">
+					<img src="http://placehold.it/600x320">
+					<div class="pxs">
+						<h3 class="paragon-text primary-heading mtn">Specials</h3>
+						<p>Nam pharetra sem eget tempus porttitor. Morbi fringilla vulputate porttitor. In hac habitasse.</p>
 					</div>
-				</li>
-				<li class="gd-third gd-columns gt-half gt-columns pvs">
-					<div class="white-bg">
-						<img src="http://placehold.it/600x320">
-
-
-						<div class="pxs">
-							<h3 class="paragon-text primary-heading mtn">Specials</h3>
-							<p>Nam pharetra sem eget tempus porttitor. Morbi fringilla vulputate porttitor. In hac habitasse.</p>
-						</div>
-					</div>
-				</li>
+				</div>
+			</li>
 			
 		</ul>
 	</div>
