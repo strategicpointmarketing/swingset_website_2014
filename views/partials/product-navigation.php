60a4a49a8d00bfbf11c7588460b96ceb59c27c27
Brian Kelley <Mac@Brians-MacBook-Pro.local>
2014-02-14 09:45:30 -0600

    product nav commit w/e

--- views/partials/product-navigation.php
+++ views/partials/product-navigation.php
@@ -1,21 +1,22 @@
-<ul class="product-navigation">
-	<li><a href="#">Swingsets</a>
-		<ul class="active">
-			<li><a href="#">Cedar Playgrounds</a>
-			<li><a href="#">Promotional Sets</a>
-			<li><a href="#">Redwood Playgrounds</a>
-			<li><a href="#">Space Saver Swingsets</a>
-			<li><a href="#">Vinyl Swingsets</a>
-			<li><a href="#">Accessories</a>
-			<li><a href="#">Level Dry</a>
-		</ul>
-	<li><a href="#">Level Dry System</a></li>
-	<li><a href="#">Trampolines</a></li>
-	<li><a href="#">Playground Surfacing</a></li>
-	<li><a href="#">Goalsetters Basketball</a></li>
-	<li><a href="#">Sheds</a></li>
-	<li><a href="#">Shop Online</a></li>
-	<li><a href="#">Drop Ship Playsets</a></li>
-	<li><a href="#">Outdoor Furniture</a></li>
-	<li><a href="#">Playhouses</a></li>
-</ul>
+<nav>
+	<ul class="product-navigation">
+		<li><a href="#">Swingsets</a>
+			<ul class="active">
+				<li><a href="#">Cedar Playgrounds</a>
+				<li><a href="#">Promotional Sets</a>
+				<li><a href="#">Redwood Playgrounds</a>
+				<li><a href="#">Space Saver Swingsets</a>
+				<li><a href="#">Vinyl Swingsets</a>
+				<li><a href="#">Accessories</a>
+				<li><a href="#">Level Dry</a>
+			</ul>
+		<li><a href="#">Level Dry System</a></li>
+		<li><a href="#">Trampolines</a></li>
+		<li><a href="#">Playground Surfacing</a></li>
+		<li><a href="#">Goalsetters Basketball</a></li>
+		<li><a href="#">Sheds</a></li>
+		<li><a href="#">Shop Online</a></li>
+		<li><a href="#">Drop Ship Playsets</a></li>
+		<li><a href="#">Outdoor Furniture</a></li>
+		<li><a href="#">Playhouses</a></li>
+	</ul>
